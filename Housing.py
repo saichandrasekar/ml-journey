d0ed3a12f31fbb198dcb84c0993c37489f2d28bf
saichandrasekar <saichandrasekar@gmail.com>
2020-12-18 07:26:16 +0530

    added more train test model preparation

--- Housing.py
+++ Housing.py
@@ -5,6 +5,10 @@ import urllib.request
 import pandas as pd
 
 import matplotlib.pyplot as plt
+import numpy as np
+from zlib import crc32
+
+from sklearn.model_selection import train_test_split
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
@@ -28,17 +32,58 @@ def load_housing_data(housing_path=HOUSING_PATH):
 # Actual program starts
 
 # Fetch the dataset
-#fetch_housing_data()
+# fetch_housing_data()
 
 # Load dataset to pandas
 housing = load_housing_data()
-#print(housing.head())
+# print(housing.head())
 
-#print(housing.info())
+# print(housing.info())
 value_counts = housing['ocean_proximity'].value_counts()
-#print(value_counts)
+# print(value_counts)
 
-#print(housing.describe())
+# print(housing.describe())
 
-housing.hist(bins=50, figsize=(20,15))
-plt.show()
+#housing.hist(bins=50, figsize=(20, 15))
+#plt.show()
+
+
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+
+train_set, test_set = split_train_test(housing, 0.2)
+print(len(train_set))
+print(len(test_set))
+
+
+def test_set_check(identifier, test_ratio):
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2 ** 32
+
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+
+housing_with_id = housing.reset_index()
+train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
+
+print(len(train_set))
+print(len(test_set))
+
+housing_with_id["id"] = housing["longitude"] * 1000 + housing["latitude"]
+train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "id")
+
+print(len(train_set))
+print(len(test_set))
+
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+
+print(len(train_set))
+print(len(test_set))
